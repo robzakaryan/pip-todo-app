3141d645e9454512b9dbe1573c62cea60fff76e8
robzakaryan <robzakaryan@gmail.com>
2024-03-28 20:43:47 +0400

    add build and dist to gitignore

--- gui.py
+++ gui.py
@@ -11,11 +11,11 @@ Gui.theme('BlueMono')
 label_clock = Gui.Text('', key='clock')
 label = Gui.Text('Type in a to-do')
 input_box = Gui.InputText(tooltip='Enter a to-do', key='todo')
-add_button = Gui.Button(key='add', tooltip='Add', image_source='images/add.png', mouseover_colors='lightblue')
+add_button = Gui.Button('Add', key='add', tooltip='Add', mouseover_colors='lightblue')
 list_box = Gui.Listbox(values=functions.get_todos(), key='todos',
                        enable_events=True, size=(45, 10))
 edit_button = Gui.Button('Edit', key='edit')
-complete_button = Gui.Button(key='complete', tooltip='Complete', image_source='images/complete.png',
+complete_button = Gui.Button('Complete', key='complete', tooltip='Complete',
                              mouseover_colors='lightblue')
 exit_button = Gui.Button('Exit', key='exit', button_color='red')
 
@@ -31,6 +31,8 @@ window = Gui.Window('My To-do app',
                     )
 while True:
     event, values = window.read(timeout=500)
+    if event == Gui.WIN_CLOSED or event == 'exit':
+        break
     window['clock'].update(value=time.strftime('%d %b, %Y %H:%M:%S'))
     match event:
         case 'add':
@@ -65,6 +67,4 @@ while True:
                 Gui.popup('select item first', font=('Helvetica', 10), text_color='red', background_color='white')
         case 'todos':
             window['todo'].update(value=values['todos'][0])
-        case Gui.WIN_CLOSED | 'exit':
-            break
 window.close()
