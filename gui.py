f9c8caf0eabd9f9cc43d1e12f1c4bcc912486378
robzakaryan <robzakaryan@gmail.com>
2024-03-28 20:25:25 +0400

    add todo to gitignore

--- gui.py
+++ gui.py
@@ -1,6 +1,11 @@
 import functions
 import PySimpleGUI as Gui
 import time
+import os
+
+if not os.path.exists('todos.txt'):
+    with open('todos.txt', 'w') as file:
+        pass
 
 Gui.theme('BlueMono')
 label_clock = Gui.Text('', key='clock')
